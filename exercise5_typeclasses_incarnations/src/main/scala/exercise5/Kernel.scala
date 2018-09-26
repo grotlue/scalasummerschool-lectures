9209285e875e42767e6fd33814abda8ed86fed00
Laura Bergenthal-Grotlüschen <laura.grotlueschen@xing.com>
2018-09-26 17:11:01 +0200

    Typeclasses exercise

--- exercise5_typeclasses_incarnations/src/main/scala/exercise5/Kernel.scala
+++ exercise5_typeclasses_incarnations/src/main/scala/exercise5/Kernel.scala
@@ -4,15 +4,15 @@ import cats.kernel._
 import Comparison._
 
 /** Short type class summary:
-  * 
+  *
   * trait Eq[A] {
   *   def eqv(x: A, y: A): Boolean = ???
   * }
-  * 
+  *
   * trait Order[A] {
   *   def compare(x: A, y: A): Int = ???
   * }
-  * 
+  *
   * trait Semigroup[A] {
   *   def combine(x: A, y: A): A = ???
   * }
@@ -21,24 +21,36 @@ object Kernel {
 
   // a) Implement an implicit Eq[String] instance.
 
+  implicit val eqvString = new Eq[String] {
+    def eqv(x: String, y: String): Boolean = x == y
+  }
 
-  def testEqString(x: String, y: String): Boolean = false
+  def testEqString(x: String, y: String): Boolean = Eq.eqv(x, y)
 
   // b) Implement an implicit Order[Int] instance.
 
+  implicit val comparisionInt = new Order[Int] {
+    def compare(x: Int, y: Int): Int = x - y
+  }
 
-  def testCompareInt(x: Int, y: Int): Comparison = GreaterThan
+  def testCompareInt(x: Int, y: Int): Comparison = Order.comparison(x, y)
 
   // c) Implement an implicit Semigroup[List[Int]] instance.
 
+//  implicit val semiList = new Semigroup[List[Int]] {
+//    def combine(x: List[Int], y: List[Int]): List[Int] = x ::: y
+//  }
 
-  def testCombineLists(x: List[Int], y: List[Int]): List[Int] = x
+  def testCombineLists(x: List[Int], y: List[Int]): List[Int] =
+    Semigroup.combine(x, y)
 
   // d) Update your List Semigroup such that it works for any element type, e.g. List[Int], List[String], ...
 
+  implicit def semiListArb[A]: Semigroup[List[A]] = new Semigroup[List[A]] {
+    def combine(x: List[A], y: List[A]): List[A] = x ::: y
+  }
 
-  def testCombineListsArb[A](x: List[A], y: List[A]): List[A] = x
-
-
+  def testCombineListsArb[A](x: List[A], y: List[A]): List[A] =
+    Semigroup.combine(x, y)
   // e) Can you find a type class in kernel which adds an empty element to Semigroup? How is it called?
 }
