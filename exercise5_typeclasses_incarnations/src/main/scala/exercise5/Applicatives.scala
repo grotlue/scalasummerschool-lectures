ea40bf0720bd8a5e0d61d054dad9aefb5586afa6
Laura Bergenthal-Grotlüschen <laura.grotlueschen@xing.com>
2018-09-27 10:51:19 +0200

    Applicatices exercise

--- exercise5_typeclasses_incarnations/src/main/scala/exercise5/Applicatives.scala
+++ exercise5_typeclasses_incarnations/src/main/scala/exercise5/Applicatives.scala
@@ -5,7 +5,7 @@ import cats.Applicative
 /** An Applicative trait has the following abstract methods:
   *   new Applicative[List] {
   *     def pure[A](a: A): List[A] = ???
-  * 
+  *
   *     def ap[A, B](ff: List[A => B])(fa: List[A]): List[B] = ???
   *   }
   */
@@ -13,10 +13,24 @@ object Applicatives {
 
   // a) Implement an implicit Applicative[List] instance.
 
+  implicit val applicativeList = new Applicative[List] {
+    def pure[A](x: A): List[A] = List(x)
 
-  def testListApplicative[A, B](ff: List[A => B], fa: List[A]): List[B] = Nil
+    def ap[A, B](ff: List[A => B])(fa: List[A]): List[B] = {
+      for {
+        a <- fa
+        f <- ff
+      } yield f(a)
+    }
+  }
+
+  def testListApplicative[A, B](ff: List[A => B], fa: List[A]): List[B] =
+    Applicative[List].ap(ff)(fa)
 
   // b) Apply all parameters to the function.
 
-  def testApplyApp(f: Int => Int => String, x: List[Int], y: List[Int]): List[String] = Nil
+  def testApplyApp(f: Int => Int => String,
+                   x: List[Int],
+                   y: List[Int]): List[String] =
+    Applicative[List].ap(x.map(f))(y)
 }
