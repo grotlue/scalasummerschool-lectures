61d2fad4d03541301613d2815902d1d28050d6b9
Laura Bergenthal-Grotlüschen <laura.grotlueschen@xing.com>
2018-09-27 11:45:50 +0200

    Monad exercise

--- exercise5_typeclasses_incarnations/src/main/scala/exercise5/Monads.scala
+++ exercise5_typeclasses_incarnations/src/main/scala/exercise5/Monads.scala
@@ -5,9 +5,9 @@ import cats.Monad
 /** A Monad trait has the following abstract methods:
   *   new Monad[List] {
   *     def pure[A](a: A): List[A] = ???
-  * 
+  *
   *     def flatMap[A, B](fa: List[A])(f: A => List[B]): List[B] = ???
-  * 
+  *
   *     // here you have to reduce `f(a)` to `List[B]` by ignoring `Left` values
   *     def tailRecM[A, B](a: A)(f: A => List[Either[A,B]]): List[B] = ???
   *   }
@@ -15,7 +15,21 @@ import cats.Monad
 object Monads {
 
   // a) Implement an implicit Monad[List] instance.
+  implicit val listMonad = new Monad[List] {
+    def pure[A](a: A): List[A] = List(a)
 
+    def flatMap[A, B](fa: List[A])(f: A => List[B]): List[B] = fa.flatMap(f)
 
-  def testListMonad[A, B](fa: List[A], f: A => List[B]): List[B] = Nil
+    def tailRecM[A, B](a: A)(f: A => List[Either[A, B]]): List[B] = {
+      f(a).foldLeft(List.empty[B]) { (acc, item) =>
+        item match {
+          case Right(v) => v +: acc
+          case _        => acc
+        }
+      }
+    }
+  }
+
+  def testListMonad[A, B](fa: List[A], f: A => List[B]): List[B] =
+    Monad[List].flatMap(fa)(f)
 }
